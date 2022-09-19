b6dd8944ec0c6985c17770e87b3c0504f70e04ca
jclar <jacob.clark@my.jcu.edu.au>
2022-09-19 16:35:07 +1000

    Refactor password check program to use functions

--- cp1404practicals/prac_02/password_stars.py
+++ cp1404practicals/prac_02/password_stars.py
@@ -1,8 +1,21 @@
-required_length = 6
-password = input("Enter password: ")
-while len(password) < 6:
-    print("Password too short")
+REQUIRED_LENGTH = 6
+
+
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Enter password: ")
+    while len(password) < REQUIRED_LENGTH:
+        print("Password too short")
+        password = input("Enter password: ")
+    return password
 
-print("*" * len(password))
 
+main()
