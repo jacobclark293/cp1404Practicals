5de23f362dfd953a9aba5b6e333aaa07ca6c01a3
jclar <jacob.clark@my.jcu.edu.au>
2022-09-27 11:25:44 +1000

    Write code to produce random number

---
+++ cp1404practicals/prac_03/randoms.py
@@ -0,0 +1,12 @@
+# Line 1: 5 is the smallest number I could've seen, 20 is the largest.
+
+# Line 2: Smallest no.: 3, Largest no,: 9  Line 2 could not produce a 4
+
+# Line 3: Smallest no.: 2.5, Largest no.: 5.5
+
+
+"""
+Write code, not a comment, to produce a random number between 1 and 100 inclusive
+"""
+import random
+print(random.randint(1, 100))
