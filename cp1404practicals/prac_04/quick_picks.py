e10f9d721c1023c28f96e96c8fab7f9f6671930e
jclar <jacob.clark@my.jcu.edu.au>
2022-10-04 14:39:53 +1000

    Create quick pick lottery ticket generator

---
+++ cp1404practicals/prac_04/quick_picks.py
@@ -0,0 +1,21 @@
+import random
+
+NUMBERS_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
+
+quick_picks_amount = int(input("How many quick picks?: "))
+while quick_picks_amount < 0:
+    print("Value must be above 0. ")
+    quick_picks_amount = int(input("How many quick picks?: "))
+
+for i in range(quick_picks_amount):
+    quick_pick = []
+    for j in range(NUMBERS_PER_LINE):
+        number = random.randint(MINIMUM, MAXIMUM)
+        while number in quick_pick:
+            number = random.randint(MINIMUM, MAXIMUM)
+        quick_pick.append(number)
+    quick_pick.sort()
+    print(" ". join("{:2}".format(number) for number in quick_pick))
