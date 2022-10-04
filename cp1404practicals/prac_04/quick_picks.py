7e9b29a68ca20d35e77ecdc2f96c12ece92386c1
jclar <jacob.clark@my.jcu.edu.au>
2022-10-04 14:42:57 +1000

    Added main and docstring

--- cp1404practicals/prac_04/quick_picks.py
+++ cp1404practicals/prac_04/quick_picks.py
@@ -5,17 +5,22 @@ MINIMUM = 1
 MAXIMUM = 45
 
 
-quick_picks_amount = int(input("How many quick picks?: "))
-while quick_picks_amount < 0:
-    print("Value must be above 0. ")
+def main():
+    """ Quick picks lottery ticket generator - choose random sets of numbers"""
     quick_picks_amount = int(input("How many quick picks?: "))
+    while quick_picks_amount < 0:
+        print("Value must be above 0. ")
+        quick_picks_amount = int(input("How many quick picks?: "))
 
-for i in range(quick_picks_amount):
-    quick_pick = []
-    for j in range(NUMBERS_PER_LINE):
-        number = random.randint(MINIMUM, MAXIMUM)
-        while number in quick_pick:
+    for i in range(quick_picks_amount):
+        quick_pick = []
+        for j in range(NUMBERS_PER_LINE):
             number = random.randint(MINIMUM, MAXIMUM)
-        quick_pick.append(number)
-    quick_pick.sort()
-    print(" ". join("{:2}".format(number) for number in quick_pick))
+            while number in quick_pick:
+                number = random.randint(MINIMUM, MAXIMUM)
+            quick_pick.append(number)
+        quick_pick.sort()
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
+
+main()
