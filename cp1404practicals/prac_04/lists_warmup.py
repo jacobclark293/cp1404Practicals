06816a0ec57f8be09bad65cd18cf5756dc03ad0b
jclar <jacob.clark@my.jcu.edu.au>
2022-10-04 14:25:27 +1000

    Clean code to better answer questions

--- cp1404practicals/prac_04/lists_warmup.py
+++ cp1404practicals/prac_04/lists_warmup.py
@@ -13,16 +13,12 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 
 # 1. Change the first element of numbers to ten (string)
 numbers[0] = "ten"
-print(numbers)
 
 # 2. Change the last element of numbers to 1
 numbers[-1] = 1
-print(numbers)
 
 # 3. Get all the elements from numbers except the first two (slice)
 del numbers[0:2]
-print(numbers)
 
 # 4. Check if 9 is an element of numbers
-if 9 in numbers:
-    print("9 is an element of numbers")
+9 in numbers
