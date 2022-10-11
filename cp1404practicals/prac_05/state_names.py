f44546f4f2834c34b4bb90464184e67931b90d58
jclar <jacob.clark@my.jcu.edu.au>
2022-10-11 16:47:08 +1000

    Add version with exceptions

--- cp1404practicals/prac_05/state_names.py
+++ cp1404practicals/prac_05/state_names.py
@@ -18,5 +18,15 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
 #     state_code = input("Enter short state: ").upper()
 
 
+print(CODE_TO_NAME)
+
+state_code = input("Enter short state: ").upper()
+while state_code != "":
+    try:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    except KeyError:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").upper()
+
 for state in CODE_TO_NAME:
     print(f"{state:3} is {CODE_TO_NAME[state]:10}")
