d63c7cd314a21d0bad12e010ce7af8d51f255021
jclar <jacob.clark@my.jcu.edu.au>
2022-10-28 12:13:07 +1000

    Initial Commit

---
+++ cp1404practicals/prac_06/languages.py
@@ -0,0 +1,8 @@
+"""
+Prac 06
+Intermediate Exercises
+Programming languages class exercise
+
+Estimated time for completion: 25 mins
+Current time: 12:11pm
+"""
---
+++ cp1404practicals/prac_06/programming_language.py
@@ -0,0 +1,3 @@
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
