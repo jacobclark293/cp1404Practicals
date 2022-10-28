a6d310a85419d0bfaca829af25c7cee530a72fe3
jclar <jacob.clark@my.jcu.edu.au>
2022-10-28 12:25:00 +1000

    Complete requirements for prac_06

--- cp1404practicals/prac_06/languages.py
+++ cp1404practicals/prac_06/languages.py
@@ -5,4 +5,20 @@ Programming languages class exercise
 
 Estimated time for completion: 25 mins
 Current time: 12:11pm
+Time when completed: 12:24pm
+Time taken: 13 minutes
 """
+
+from programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+
+languages = [python, ruby, visual_basic]
+# print(languages)
+print("The dynamically typed languages are:")
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
--- cp1404practicals/prac_06/programming_language.py
+++ cp1404practicals/prac_06/programming_language.py
@@ -1,3 +1,17 @@
-
 class ProgrammingLanguage:
+    """Represent information about a programming language"""
+
     def __init__(self, name, typing, reflection, year):
+        """Construct a ProgrammingLanguage from the given values"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Return string representation of a ProgrammingLanguage"""
+        return f"{self.name}, {self.typing} Typing, reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Determine if language is dynamic"""
+        return self.typing == "Dynamic"
