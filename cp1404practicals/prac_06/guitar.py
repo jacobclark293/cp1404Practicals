47941773e098081c5122ce6234c4b40f6dd4d341
jclar <jacob.clark@my.jcu.edu.au>
2022-10-28 12:43:41 +1000

    Complete writing Guitar class

--- cp1404practicals/prac_06/guitar.py
+++ cp1404practicals/prac_06/guitar.py
@@ -1,3 +1,20 @@
 class Guitar:
     """Represent information about a guitar"""
-    def __init__(self):
+
+    def __init__(self, name, year, cost):
+        """Construct a guitar from the given values"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return string representation of Guitar"""
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def get_age(self):
+        """Determine how old a guitar is"""
+        return 2022 - self.year
+
+    def is_vintage(self):
+        """Determine if a guitar is vintage or not"""
+        return self.get_age() >= 50
