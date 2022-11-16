06b224637f486efe7e50330ccad8e40623ed0bc8
jclar <jacob.clark@my.jcu.edu.au>
2022-11-16 12:36:10 +1000

    Add missing docstring

--- cp1404practicals/prac_09/unreliable_car.py
+++ cp1404practicals/prac_09/unreliable_car.py
@@ -1,16 +1,18 @@
 from car import Car
 from random import randint
 
-class UnreliableCar(Car):
 
-    def __init__(self,  name, fuel, reliability):
+class UnreliableCar(Car):
+    """Unreliable version of car class"""
+
+    def __init__(self, name, fuel, reliability):
         """Initialise an UnreliableCar"""
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
         """Drive car based on reliability"""
-        random_number = randint(1,100)
+        random_number = randint(1, 100)
         if random_number >= self.reliability:
             distance = 0
         distance_driven = super().drive(distance)
