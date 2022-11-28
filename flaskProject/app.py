c235531c3f7a20740669e56b96fcfff8558efbde
jclar <jacob.clark@my.jcu.edu.au>
2022-11-28 19:32:55 +1000

    test commit

---
+++ flaskProject/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return '<h1>Hello World :)</h1>'
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=''):
+    return f'Hello {name}'
+
+
+@app.route('/f')
+@app.route('/f/<celsius>')
+def convert_to_fahrenheit(celsius=''):
+    celsius = float(celsius)
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return f"{fahrenheit}"
+
+if __name__ == '__main__':
+    app.run()
