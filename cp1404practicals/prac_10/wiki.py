6af63447cd325c0937f8c139efaeebe283455cd0
jclar <jacob.clark@my.jcu.edu.au>
2022-11-28 17:04:42 +1000

    Complete wiki program

---
+++ cp1404practicals/prac_10/wiki.py
@@ -0,0 +1,14 @@
+import wikipedia
+
+search = input("Enter page title: ")
+wiki_page = wikipedia.page(search)
+while search != '':
+    try:
+        print(wiki_page.title)
+        print(wiki_page.url)
+        print(wikipedia.summary(search))
+
+    except wikipedia.exceptions.DisambiguationError as e:
+        print(e.options)
+    search = input("Enter page title: ")
+
